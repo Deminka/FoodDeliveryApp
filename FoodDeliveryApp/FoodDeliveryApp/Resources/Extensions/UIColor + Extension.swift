20ef5db0ca3995d6cd136c5cca4cee34b5f5ad09
Dmitry Komissarov <komdimkom@gmail.com>
2024-02-01 12:36:20 +0300

    Added onboarding coordinator

--- FoodDeliveryApp/FoodDeliveryApp/Resources/Extensions/UIColor + Extension.swift
+++ FoodDeliveryApp/FoodDeliveryApp/Resources/Extensions/UIColor + Extension.swift
@@ -8,11 +8,12 @@
 import UIKit
 
 extension UIColor {
-    
     func hex(_ rgbValue: UInt64) -> UIColor {
-        return UIColor(red: ((rgbValue & 0xFF0000) >> 16) / 255.0,
-                       green: ((rgbValue & 0xFF0000) >> 8) / 255.0,
-                       blue: (rgbValue & 0xFF0000) / 255.0,
-                       alpha: CGFloat(1.0))
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
 }
