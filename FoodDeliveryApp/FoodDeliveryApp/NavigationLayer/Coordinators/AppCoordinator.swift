8d32b70cf51f4fad1d4eb819cbb38cca077f4a5b
Dmitry Komissarov <komdimkom@gmail.com>
2024-03-25 16:41:53 +0300

    Added auth scenes states, connected login scenes to coordinator and flow

--- FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/AppCoordinator.swift
+++ FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/AppCoordinator.swift
@@ -14,12 +14,10 @@ class AppCoordinator: Coordinator {
 
     override func start() {
         if userStorage.passedOnboarding {
-            showMainFlow()
+            showAuthFlow()
         } else {
             showOnboardingFlow()
         }
-//        let loginVC = LoginViewController()
-//        navigationController?.pushViewController(loginVC, animated: true)
     }
     
     override func finish() {
@@ -38,6 +36,26 @@ private extension AppCoordinator {
         let tabBarController = factory.makeMainFlow(coordinator: self, finishDelegate: self)
          navigationController.pushViewController(tabBarController, animated: true)
     }
+    func showAuthFlow() {
+        guard let navigationController = navigationController else { return }
+        let vc = factory.makeAuthScene(coordinator: self)
+        navigationController.pushViewController(vc, animated: true)
+    }
+}
+
+//MARK: - Methods
+extension AppCoordinator {
+    func showSignInScene() {
+        guard let navigationController = navigationController else { return }
+        let vc = factory.makeSignInScene(coordinator: self)
+        navigationController.pushViewController(vc, animated: true)
+    }
+    func showSignUpScene() {
+        guard let navigationController = navigationController else { return }
+        let vc = factory.makeSignUpScene(coordinator: self)
+        navigationController.pushViewController(vc, animated: true)
+    }
+    
 }
 
 extension AppCoordinator: CoordinatorFinishDelegate {
@@ -48,7 +66,7 @@ extension AppCoordinator: CoordinatorFinishDelegate {
         switch childCoordinators.type {
         case .onboarding:
             navigationController?.viewControllers.removeAll()
-            showMainFlow()
+            showAuthFlow()
         case .app:
             return
         default:
